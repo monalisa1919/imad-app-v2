4a1495351b3e009264f68916a7c518cc8b312715
monalisa1919 <monalisa.kk19@gmail.com>
2017-03-03 12:22:37 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,10 @@ console.log('Loaded!');
 
 //change the text of main-text dive
 var element = document.getElementById('main-text');
+element.innerHTML="Things are getting clear by time :P";
 
-element.innerHTML="Things are getting clear by time :P"
+//make an element move
+var img = document.getElementById('img');
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+};
